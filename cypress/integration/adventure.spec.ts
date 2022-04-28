f5c327773615b7ceb46b1bb2286973a40a6fe2a6
Laura <laura97green@gmail.com>
2022-04-28 20:50:10 +0200

    initial commit

---
+++ cypress/integration/adventure.spec.ts
@@ -0,0 +1,34 @@
+import { HomePage } from '../pages/home.page';
+import { AdventureDetailsPage } from '../pages/adventure-details.page';
+
+describe('Adventure', () => {
+    const homePage = new HomePage();
+    const adventureDetailsPage = new AdventureDetailsPage();
+
+    before(() => {
+        cy.deleteAdventure(4);
+        cy.createAdventure(4, 'New Adventure');
+    })
+
+    it('should visit CarvedRock homepage', () => {
+        homePage.visit();
+    });
+
+    it('should open the New adventure', () => {
+        homePage.clickMoreDetailsBtn(4)
+        .getAdventureTitle().should('have.text', 'New Adventure');
+    });
+    it('should post a comment', () => {
+        adventureDetailsPage.resetComments()
+        .addComment('Laura', 'What a great adventure!')
+        .getLastComment().then($el => {
+            cy.wrap($el).find('p').should('have.text', 'What a great adventure!');
+            cy.wrap($el).find('footer').should('have.text', 'Laura')
+        })
+})
+    it('should not post a comment if the comment text is missing', () => {
+        adventureDetailsPage.addComment('Laura', '')
+            .getCommentFiledValidationError()
+            .should('have.text', 'Comment is required.');
+})
+})
---
+++ cypress/integration/filter.spec.ts
@@ -0,0 +1,9 @@
+describe('Filter', () => {
+    it('should display filter criteria', () => {
+        cy.visit('/')
+            .filterAdventures('Tara');
+
+        cy.get('p[data-test-automation="filtered-by"]')
+            .should('have.text', 'Filtered by: Tara')
+    });
+});
